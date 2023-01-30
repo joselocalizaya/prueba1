32f7afd98d68c6a33453ca4439833c9c6200806c
joselocalizaya <grafitek5tudio@outlook.com>
2023-01-30 23:42:22 +0100

    second commit

--- src/app/views/listado/entrada/entrada.component.ts
+++ src/app/views/listado/entrada/entrada.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Entrada } from 'src/app/shared/interfaces/interfaces';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-entrada',
   templateUrl: './entrada.component.html',
   styleUrls: ['./entrada.component.css']
 })
-export class EntradaComponent {
+export class EntradaComponent implements OnInit{
+  @Input()
+  public entrada!: Entrada;
 
+    constructor() {}
+
+    ngOnInit(): void {
+
+    }
 }
--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Entrada } from './../../shared/interfaces/interfaces';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
-export class ListadoComponent {
+export class ListadoComponent implements OnInit{
+  public listadoEntradas: Entrada[];
 
+  constructor() {
+    this.listadoEntradas = [
+      {
+        titulo: 'Introduccion a Angular',
+        resumen: 'En esta leccion realizaremos una pequeña introduccion'
+      },
+      {
+        titulo: 'Typescript como lenguaje para Angular',
+        resumen: 'Breve recorrido por el lenguaje Typescript'
+      },
+      {
+        titulo: 'Componenetes en Angular',
+        resumen: 'Aprenderemos a usar los componenetes'
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  public mostrarTitulo(titulo: string):void {
+    alert(`Entrada seleccionada: ${ titulo }.`);
+  }
 }
